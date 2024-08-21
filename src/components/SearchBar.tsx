eb077a00949bdd6c97b8eb67ad83c22b54aed782
DougMoraes <dougmoraes91@gmail.com>
2024-08-21 19:40:56 +0200

    feat: OnPress search navigate to ResultsScreen

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import { View, TextInput, StyleSheet } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 import Button from './Button';
 
 export default function SearchBar() {
+  const navigation = useNavigation();
+
   return (
     <View style={styles.container}>
       <TextInput
@@ -11,6 +14,7 @@ export default function SearchBar() {
       <Button
         text="Search"
         style={styles.button}
+        onPress={() => navigation.navigate('Results', {searchText: 'Gabo'})}
       />
     </View>
   );
