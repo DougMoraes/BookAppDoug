ee8de7bbbeb85bc2dcd088effea203b2beb3b128
DougMoraes <dougmoraes91@gmail.com>
2024-08-27 18:08:28 +0200

    feat: add fetchBooks

--- src/features/books/booksSlice.tsx
+++ src/features/books/booksSlice.tsx
@@ -1,30 +1,57 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import { Book } from '../../types';
 
-const MockBook = {
-  id: '1',
-  title: 'One Title',
-  author: 'One Author',
-};
-
 export interface BooksState {
-  results: Book[]
+  results: Book[],
+  searchStatus: 'ready' | 'loading' | 'failed',
 }
 
 const initialState: BooksState = {
   results: [],
+  searchStatus: 'ready',
 };
+
+export const fetchBooks = createAsyncThunk(
+  'books/fetchBooks',
+  async (text: string) => {
+    const url = `https://openlibrary.org/search.json?q=${text}`;
+
+    try {
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error(`Response status: ${response.status}`);
+      }
+
+      const json = await response.json();
+      return json.docs;
+    } catch (error) {
+      if (error instanceof Error) {
+        console.log(error.message);
+      } else {
+          throw error;
+      }
+    }
+  }
+);
 
 export const booksSlice = createSlice({
   name: 'books',
   initialState,
-  reducers: {
-    searchBooks: (state) => {
-      state.results = [MockBook];
-    },
+  reducers: {},
+  extraReducers: builder => {
+    builder.addCase(fetchBooks.pending, state => {
+      state.searchStatus = 'loading';
+      state.results = [];
+    });
+    builder.addCase(fetchBooks.fulfilled, (state, action) => {
+      state.searchStatus = 'ready';
+      state.results = action.payload;
+    });
+    builder.addCase(fetchBooks.rejected, state => {
+      state.searchStatus = 'failed';
+      state.results = [];
+    });
   },
 });
-
-export const { searchBooks } = booksSlice.actions;
 
 export default booksSlice.reducer;
