bbde5eafba34c8182358fc09d0aa5588c22c3a32
DougMoraes <dougmoraes91@gmail.com>
2024-08-28 16:18:26 +0200

    chore: add missing jest config

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,15 @@
 module.exports = {
   preset: 'react-native',
+  setupFiles: [
+    './node_modules/react-native-gesture-handler/jestSetup.js',
+  ],
   testPathIgnorePatterns: [
     '/node_modules/',
     '/__utils__/',
   ],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   transformIgnorePatterns: [
-    'node_modules/(?!(react-native|@react-native|@react-navigation|react-navigation-stack)/)',
+    'node_modules/(?!(react-native|@react-native|@react-navigation|react-navigation-stack|react-redux)/)',
   ],
   transform: {
     '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
