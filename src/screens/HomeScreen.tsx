4eee147a39307550b63a444b441960ee53f833dc
DougMoraes <dougmoraes91@gmail.com>
2024-09-03 20:13:27 +0200

    feat: Add error handling for searchText less than 3 chars

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   SafeAreaView,
   StatusBar,
   StyleSheet,
+  Text,
   useColorScheme,
 } from 'react-native';
 import { Colors } from 'react-native/Libraries/NewAppScreen';
@@ -13,13 +14,19 @@ import { RootStackParamList } from '../types';
 type HomeScreenProps = NativeStackScreenProps<RootStackParamList, 'Home'>;
 
 function HomeScreen({navigation}: HomeScreenProps): React.JSX.Element {
+  const [error, setError] = useState<string | null>(null);
   const isDarkMode = useColorScheme() === 'dark';
 
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
 
-  const onPressSearch = (text: string) => {
+  const onPressSearch = (text: string | null) => {
+    if (!text || text.length < 3) {
+      setError('Search text must have at least 3 characters');
+      return;
+    }
+    setError(null);
     navigation.navigate('Results', {searchText: text});
   };
 
@@ -30,6 +37,7 @@ function HomeScreen({navigation}: HomeScreenProps): React.JSX.Element {
         backgroundColor={backgroundStyle.backgroundColor}
       />
       <SearchBar onPressSearch={onPressSearch}/>
+      <Text style={styles.errorText}>{error}</Text>
     </SafeAreaView>
   );
 }
@@ -41,5 +49,10 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  errorText: {
+    color: 'red',
+    fontSize: 16,
+    fontWeight: 'bold',
   },
 });
--- src/screens/__tests__/HomeScreen.test.tsx
+++ src/screens/__tests__/HomeScreen.test.tsx
@@ -3,6 +3,7 @@ import HomeScreen from "../HomeScreen";
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { RootStackParamList } from '../../types';
 import { renderWithProviders } from '../../__tests__/__utils__/utils';
+import { screen, userEvent } from '@testing-library/react-native';
 
 describe('HomeScreen', () => {
   test('should render correctly', () => {
@@ -15,5 +16,30 @@ describe('HomeScreen', () => {
     ).toJSON();
 
     expect(tree).toMatchSnapshot();
+  });
+
+  test('should show error message if Search text < 3', async () => {
+    const user = userEvent.setup({
+      advanceTimers: () => jest.advanceTimersToNextTimer(),
+    });
+    const Stack = createNativeStackNavigator<RootStackParamList>();
+
+    renderWithProviders(
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    );
+
+    const renderedSearchButton = screen.getAllByText('Search');
+    const renderedErrorMessagePrePress = screen.queryByText('Search text must have at least 3 characters');
+
+    expect(renderedSearchButton.length).toBe(1);
+    expect(renderedErrorMessagePrePress).toBeFalsy();
+
+    await user.press(renderedSearchButton[0]);
+
+    const renderedErrorMessagePostPress = screen.getAllByText('Search text must have at least 3 characters');
+
+    expect(renderedErrorMessagePostPress.length).toBe(1);
   });
 });
